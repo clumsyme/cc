3752f5e7d19571a69a1249b6f5159fb84b79314d
李彦 <liyan@hualala.com>
2017-06-15 16:56:10 +0800

    contacts component

---
+++ cc/components/contacts/index.js
@@ -0,0 +1,25 @@
+// @ts-check
+
+import React, { Component } from 'react';
+import './styles.css'
+
+class Contacts extends Component {
+    constructor(props) {
+        super(props)
+    }
+    render() {
+        const { avatar, name } = this.props
+        return (
+            <div className="cc_contact_item" onClick={this.props.onClick}>
+                <div className="cc_contact_avatar_block">
+                    <img className="cc_contact_avatar" src={avatar} alt={name} />
+                </div>
+                <div className="cc_contact_name">
+                    <h4>{name}</h4>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Contacts
