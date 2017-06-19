e0126b595c6fe250bc7d903f7a9224f58d35f4fe
李彦 <liyan@hualala.com>
2017-06-19 16:02:10 +0800

    unread badge

--- cc/avatar/index.js
+++ cc/avatar/index.js
@@ -7,19 +7,47 @@ export default class Avatar extends Component {
         super(props)
     }
     render() {
-        const { src, name, style, size, shape, unread } = this.props
+        const { src, name, style, size, shape } = this.props
         const className = 'cc_avatar' + (shape === 'circle' ? ' circle' : '')
+        let unread = this.props.unread
+        unread = unread > 99 ? '99+' : unread
+
+        let resized
+        if (size) {
+            resized = {
+                width: size + 'px',
+                height: size + 'px',
+            }
+        }
+
         return (
             <span
                 className="cc_avatar_block"
-                style={{ ...style, width: size + 'px', height: size + 'px' }}
+                style={{
+                    ...style,
+                    ...resized,
+                }}
             >
                 <img
                     className={className}
                     src={src}
                     alt={name}
                 />
-                {unread && <sup className="cc_avatar_unread">{(!size || size > 40) && unread}</sup>}
+                {
+                    unread
+                    &&
+                    (
+                        (size && size <= 40)
+                            ?
+                            <span className="cc_avatar_unread_dot"></span>
+                            :
+                            <span
+                                className="cc_avatar_unread"
+                            >
+                                {unread}
+                            </span>
+                    )
+                }
             </span>
         )
     }
